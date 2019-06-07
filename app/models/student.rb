5a854964cb02cc2b1d02d28868852cca366edf1c
ch2isk4kos <ch2isk4kos@gmail.com>
2019-06-06 23:46:32 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,8 @@
 class Student < ActiveRecord::Base
 
-    def self.to_s
-        "#{self.first_name} #{self.last_name}"
+    # instance model
+    def to_s
+        self.first_name + " " + self.last_name
     end
 
 end
