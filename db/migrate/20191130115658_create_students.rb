f797cb6805c6ec49fe8a412401fa62feed3cee96
Dwayne McFarlane <dwaynemcfarlaneny@gmail.com>
2019-11-30 07:05:44 -0500

    Done.

---
+++ db/migrate/20191130115658_create_students.rb
@@ -0,0 +1,9 @@
+class CreateStudents < ActiveRecord::Migration[5.0]
+  def change
+    create_table :students do |t|
+      t.text :first_name
+      t.text :last_name
+      t.timestamps
+    end
+  end
+end
