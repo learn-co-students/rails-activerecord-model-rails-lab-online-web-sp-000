2e176bc357767b59ea57ca4a173bcd2513e6a369
Ronnie Stephens <rstephens@bluemorpho.cc>
2019-12-29 17:38:26 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 1) do
 
   create_table "students", force: :cascade do |t|
     t.string "first_name"
-    t.string "last_name"
+    t.string "last_nam"
   end
 
 end
