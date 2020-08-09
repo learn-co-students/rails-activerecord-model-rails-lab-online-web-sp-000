942d007999eccb142382df471785434951039787
Garrett Thompson <gtcruz.gt@gmail.com>
2020-08-09 03:49:52 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
-
+ActiveRecord::Schema.define(version: 20190705214836) do
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+  end
 end
